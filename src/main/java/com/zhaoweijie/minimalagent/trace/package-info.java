205435b861416c76352b2a09cf2c000e373e6de8
zwj <892105024@qq.com>
2026-09-07 19:29:05 +0800

    创建 Spring Boot 项目骨架

---
+++ src/main/java/com/zhaoweijie/minimalagent/trace/package-info.java
@@ -0,0 +1,1 @@
+package com.zhaoweijie.minimalagent.trace;
---
+++ src/test/java/com/zhaoweijie/minimalagent/AgentApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zhaoweijie.minimalagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
